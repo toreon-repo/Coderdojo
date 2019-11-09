3d6e29617311851422dba0e5523bebe56d59f900
Craig Simonson <craig.simonson@toreon.com>
2019-11-09 12:52:52 +0100

    minor changes and cleanup

--- Arduino/Sketches/verkeerslicht/verkeerslicht.ino
+++ Arduino/Sketches/verkeerslicht/verkeerslicht.ino
@@ -1,19 +1,22 @@
+  // aangeven pin nummers op Arduino board
+  // verkeerslicht 1
   int rood1 = 8;
   int groen1 = 9;
   int blau1 = 10;
-
+  
+  // verkeerslicht 2
   int rood2 = 3;
   int groen2 = 2;
   int blau2 = 1;
 
 void setup() {
   //aangeven wat elke poort is.
-   pinMode(rood1,OUTPUT);
-   pinMode(blau1,OUTPUT);
-   pinMode(groen1,OUTPUT);
-   pinMode(rood2,OUTPUT);
-   pinMode(groen2,OUTPUT);
-   pinMode(blau2, OUTPUT);
+  pinMode(rood1,OUTPUT);
+  pinMode(blau1,OUTPUT);
+  pinMode(groen1,OUTPUT);
+  pinMode(rood2,OUTPUT);
+  pinMode(groen2,OUTPUT);
+  pinMode(blau2, OUTPUT);
 }
 
 void loop() {
@@ -39,12 +42,11 @@ void loop() {
   delay(3000);
   // GEEL (WIT)
   // DELAY (1)
-digitalWrite(groen1,HIGH);
-digitalWrite(groen2,LOW);
- digitalWrite(blau1,HIGH);
- digitalWrite(blau2,LOW);
+  digitalWrite(groen1,HIGH);
+  digitalWrite(groen2,LOW);
+  digitalWrite(blau1,HIGH);
+  digitalWrite(blau2,LOW);
   digitalWrite(rood1,HIGH);
   digitalWrite(rood2,LOW);
   delay(1000);
-
 }
